e852e9ce8edc09608784d9a746566c29cfef638d
mohammed054 <mohammed.hassoun054@gmail.com>
2026-04-01 19:42:32 +0400

    vite + deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
 import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
-  // This ensures assets load from /cooperative/ instead of the root
-  base: '/cooperative/', 
+  base: '/cooperative/',
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
